a99f78761c056b0b5e079fa226b054e6b9169844
jiho5993 <jiho5993@naver.com>
2020-09-08 23:13:17 +0900

    Add reply mod-btn & del-btn

--- pages/reply_list.js
+++ pages/reply_list.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Router from "next/router";
 import Axios from "axios";
 import Reply_list_css from "../css/reply_list_css";
 
@@ -18,11 +20,33 @@ const Reply_list = (props) => {
             });
     }, []);
 
+    const handleDel = (e) => {
+        Axios.delete(`http://localhost:3030/api/reply/delete/${e.target.id}`)
+            .then(res => {
+                alert("댓글이 삭제되었습니다.");
+                Router.push(`/article/${id}`);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
     const replyList = reply.map((re) => (
         <div className={"reply-box"} key={re.reply_no}>
             <h3 className={"reply-nickname-box"}>{re.nickname}</h3>
             <h5 className={"reply-info-box"}>{re.reg_date}</h5>
             <p className={"reply-content-box"}>{re.content}</p>
+            <Link href={{
+                pathname: '/reply_modify',
+                query: {
+                    reply_no: re.reply_no,
+                    article_no: id
+                }
+            }}
+            >
+                <button className={"btn mod-btn"}>수정</button>
+            </Link>
+            <button className={"btn del-btn"} id={re.reply_no} onClick={handleDel}>댓글 삭제</button>
         </div>
     ));
 
---
+++ pages/reply_modify.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Axios from 'axios';
+import Layout from './components/layout';
+
+const Reply_modify = ({ reply_no, article_no, content }) => {
+
+    return (
+        <Layout>
+            <p>{reply_no}</p>
+            <p>{article_no}</p>
+            <p>{content}</p>
+        </Layout>
+    )
+};
+
+Reply_modify.getInitialProps = async (req) => {
+    const { reply_no, article_no } = req.query;
+    const res = await Axios.get(`http://localhost:3030/api/reply/get_reply/${reply_no}`);
+    const data = res.data;
+
+    return {
+        reply_no,
+        article_no,
+        content: data.content
+    }
+}
+
+export default Reply_modify;
